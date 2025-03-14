05f4465fa1585eba5a0d15ebfb3cda913465232d
BuildTools <unconfigured@null.spigotmc.org>
2025-03-14 13:12:55 +0100

    mapfix

--- src/map.js
+++ src/map.js
@@ -17,12 +17,7 @@ function setMapSrc(origin, destination, waypoints, mode) {
     const apiKey = 'AIzaSyAiNShtoakde7DND2RWroZLuRIwb5isgT8';
     const baseUrl = 'https://www.google.com/maps/embed/v1/directions';
     const src = `${baseUrl}?key=${apiKey}&origin=${encodeURIComponent(origin)}&destination=${encodeURIComponent(destination)}&waypoints=${encodeURIComponent(waypoints.join('|'))}&mode=${mode}`;
-<<<<<<< HEAD
-    document.getElementById('mapFrame').src = src;
-}
 
-setMapSrc('antwerpen', 'kortenberg', ['zaventem', 'nossegem'], 'driving');
-=======
         document.getElementById('mapFrame').src = src;
     }
 
@@ -59,4 +54,3 @@ async function getTravelTime(origin, destination, apiKey) {
     return null;
     }
 }
->>>>>>> origin/main
